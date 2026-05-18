3624e2d92bbdf73aa3fc4392209dd04cf3f36318
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2026-05-17 20:44:00 -0600

    trying to work out sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://emberandoakwoodfired.netlify.app/', // ← swap per client
   integrations: [tailwind()],
+    output: 'static',
 });
