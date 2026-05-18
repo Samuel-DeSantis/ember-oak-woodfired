ced4adae4224919bd84d769d0e89a4f895e24e7e
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2026-05-17 20:48:50 -0600

    removing sitemap, poor performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://emberandoakwoodfired.netlify.app/', // ← swap per client
+  //site: 'https://emberandoakwoodfired.netlify.app/', // ← swap per client
   integrations: [tailwind()],
-    output: 'static',
+  //output: 'static',
 });
